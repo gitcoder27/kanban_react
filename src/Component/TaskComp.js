2cbb4f8d7fb7ceb8e86adbcae3179464d8787992
Ayan <ayan.mails.saha@gmail.com>
2023-02-18 21:26:42 +0530

    added bootstrap library

---
+++ src/Component/TaskComp.js
@@ -0,0 +1,7 @@
+export const TaskComp = (props) => {
+  return (
+    <div className="task">
+      <h3>{props.task}</h3>
+    </div>
+  );
+};
---
+++ src/Component/TaskGroupComp.js
@@ -0,0 +1,5 @@
+import { TaskComp } from "./TaskComp";
+
+export const TaskGroupComp = (props) => {
+  return <TaskComp task={props.tasks} />;
+};
